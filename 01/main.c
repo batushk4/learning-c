219c1d0fdbcb207fede20f6002f70daafdad8e2f
barfly <oseias.laborda@gmail.com>
2023-02-22 22:49:19 -0400

    Como fazer comentários na linguagem C

---
+++ 01/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // este é um comentário
+
+    /*
+        isso aqui
+        é um grande
+        bloco de
+        comentário
+    */
+
+    printf("Aprendendo comentários na linguagem C\n");
+    printf("Hello world!\n");
+    return 0;
+}

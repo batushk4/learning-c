334cb8896137fd4e858e8e27cb7da1b469194db7
barfly <oseias.laborda@gmail.com>
2023-02-23 21:02:57 -0400

    função printf

---
+++ 02/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// caractere de escape para saltar uma linha -> \n
+
+int main() {
+
+    // printf é uma função de saída
+    printf("\n----------------------------------\n");
+    printf("1 - Logar 2 - cadastrar 3 - Imprimir");
+    printf("\n----------------------------------\n");
+
+    printf("\nValor retornado: %d", printf("Bom\n"));
+
+    return 0;
+}
